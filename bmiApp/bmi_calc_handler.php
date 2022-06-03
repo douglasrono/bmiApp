b2eb5d2375e1708880d3de13c8822ef27623e064
Douglas Kiprono <69076570+itsdaglas@users.noreply.github.com>
2022-06-03 20:49:29 +0300

    initial commit
    A system code for calculating body mass index (BMI)

---
+++ bmiApp/bmi_calc_handler.php
@@ -0,0 +1,18 @@
+
+<?php
+if(isset(($_GET['btn_calc']))){
+    $email = $_GET['email'];
+    $password = $_GET['u_pass'];
+    $weight = $_GET['u_weight'];
+    $height = $_GET['u_height'];
+    
+    if($email == "abc@gmail.com" && $password == "123"){
+        $bmi = $weight / pow($height, exponent: 2);
+
+        echo " Hello your BMI is $bmi";
+    }
+    else{
+        echo "wrong email or password";
+    }
+}
+?>
